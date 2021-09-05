da8f89887115545e3b27ca146acc9a2fae77f6fb
Rafael Zimmermann <rafael.zimmermann@trivago.com>
2021-09-05 22:21:04 +0200

    :(

--- create_pool.sh
+++ create_pool.sh
@@ -4,75 +4,13 @@ set -ex
 
 MIN_TTL_MINUTES=1200
 
-metadata="$1"
-metada_url="$2"
-node_ip="$3"
-node_port="$4"
-path="$5"
-paymentaddr="$6"
-paymentskey="$7"
-stakevkey="$8"  # File
-stakeskey="$9"  # File
-stake_cert="${10}"
-genesis_file="${11}"
-paymentaddr2="${12}"
+path="$1"
+paymentaddr="$2"
+paymentskey="$3"
+stakeskey="$4"  # File
+stake_cert="$5"
 
-hash=$(cardano-cli stake-pool metadata-hash --pool-metadata-file $metadata)
 tmp=`mktemp -d`
-
-echo "Creating stake pool keys"
-
-cardano-cli node key-gen \
-    --cold-verification-key-file $path/cold.vkey \
-    --cold-signing-key-file $path/cold.skey \
-    --operational-certificate-issue-counter-file $path/cold.counter
-
-cardano-cli node key-gen-VRF \
-    --verification-key-file $path/vrf.vkey \
-    --signing-key-file $path/vrf.skey
-
-cardano-cli node key-gen-KES \
-    --verification-key-file $path/kes.vkey \
-    --signing-key-file $path/kes.skey
-
-slots_per_kes_period=$(cat $genesis_file | jq .slotsPerKESPeriod)
-slot=$(cardano-cli query tip --testnet-magic 1097911063 | jq .slot)
-
-keys_period=$(expr $slot / $slots_per_kes_period)
-
-cardano-cli node issue-op-cert \
---kes-verification-key-file $path/kes.vkey \
---cold-signing-key-file $path/cold.skey \
---operational-certificate-issue-counter $path/cold.counter \
---kes-period $keys_period \
---out-file $path/node.cert
-
-
-coldvkey=$path/cold.vkey
-coldskey=$path/cold.skey
-vrfkey=$path/vrf.vkey
-
-
-cardano-cli stake-pool registration-certificate \
-    --cold-verification-key-file $coldvkey \
-    --vrf-verification-key-file $vrfkey \
-    --pool-pledge 10000000 \
-    --pool-cost 340000000 \
-    --pool-margin 0.02 \
-    --pool-reward-account-verification-key-file $stakevkey \
-    --pool-owner-stake-verification-key-file $stakevkey \
-    --pool-relay-ipv4 $node_ip \
-    --pool-relay-port $node_port \
-    --metadata-url $metada_url \
-    --metadata-hash $hash \
-    --out-file $path/pool-registration.crt \
-    --testnet-magic 1097911063
-
-cardano-cli stake-address delegation-certificate \
-    --stake-verification-key-file $stakevkey \
-    --cold-verification-key-file $coldvkey \
-    --out-file $path/delegation.cert
-
 
 echo "Executing deposit"
 
@@ -84,9 +22,9 @@ echo "Amount LL: $amount_ll"
 
 cardano-cli query utxo --address $paymentaddr --testnet-magic 1097911063
 
-tx_hash=$(cardano-cli query utxo --address $paymentaddr2 --testnet-magic 1097911063 | tail -n1 | awk '{print $1}')
-tx_ix=$(cardano-cli query utxo --address $paymentaddr2 --testnet-magic 1097911063 | tail -n1 | awk '{print $2}')
-balance=$(cardano-cli query utxo --address $paymentaddr2 --testnet-magic 1097911063 | tail -n1 | awk '{print $3}')
+tx_hash=$(cardano-cli query utxo --address $paymentaddr --testnet-magic 1097911063 | tail -n1 | awk '{print $1}')
+tx_ix=$(cardano-cli query utxo --address $paymentaddr --testnet-magic 1097911063 | tail -n1 | awk '{print $2}')
+balance=$(cardano-cli query utxo --address $paymentaddr --testnet-magic 1097911063 | tail -n1 | awk '{print $3}')
 
 echo "TxHash: $tx_hash"
 echo "TxIx: $tx_ix"
---
+++ register_pool.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -ex
+
+MIN_TTL_MINUTES=1200
+
+metadata="$1"
+metada_url="$2"
+node_ip="$3"
+node_port="$4"
+path="$5"
+paymentaddr="$6"
+paymentskey="$7"
+stakevkey="$8"  # File
+
+genesis_file="${11}"
+
+echo "Creating stake pool keys"
+
+tmp=`mktemp -d`
+
+
+cardano-cli node key-gen \
+    --cold-verification-key-file $path/cold.vkey \
+    --cold-signing-key-file $path/cold.skey \
+    --operational-certificate-issue-counter-file $path/cold.counter
+
+cardano-cli node key-gen-VRF \
+    --verification-key-file $path/vrf.vkey \
+    --signing-key-file $path/vrf.skey
+
+cardano-cli node key-gen-KES \
+    --verification-key-file $path/kes.vkey \
+    --signing-key-file $path/kes.skey
+
+slots_per_kes_period=$(cat $genesis_file | jq .slotsPerKESPeriod)
+slot=$(cardano-cli query tip --testnet-magic 1097911063 | jq .slot)
+
+keys_period=$(expr $slot / $slots_per_kes_period)
+
+cardano-cli node issue-op-cert \
+--kes-verification-key-file $path/kes.vkey \
+--cold-signing-key-file $path/cold.skey \
+--operational-certificate-issue-counter $path/cold.counter \
+--kes-period $keys_period \
+--out-file $path/node.cert
+
+
+coldvkey=$path/cold.vkey
+coldskey=$path/cold.skey
+vrfkey=$path/vrf.vkey
+
+hash=$(cardano-cli stake-pool metadata-hash --pool-metadata-file $metadata)
+
+
+cardano-cli stake-pool registration-certificate \
+    --cold-verification-key-file $coldvkey \
+    --vrf-verification-key-file $vrfkey \
+    --pool-pledge 10000000 \
+    --pool-cost 340000000 \
+    --pool-margin 0.02 \
+    --pool-reward-account-verification-key-file $stakevkey \
+    --pool-owner-stake-verification-key-file $stakevkey \
+    --pool-relay-ipv4 $node_ip \
+    --pool-relay-port $node_port \
+    --metadata-url $metada_url \
+    --metadata-hash $hash \
+    --out-file $path/pool-registration.crt \
+    --testnet-magic 1097911063
+
+cardano-cli stake-address delegation-certificate \
+    --stake-verification-key-file $stakevkey \
+    --cold-verification-key-file $coldvkey \
+    --out-file $path/delegation.cert
